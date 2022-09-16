10bd1ecb8c5b5aa12af7b91211ececd5041f3133
Drummer Dee <drummer.deja@gmail.com>
2022-09-16 17:26:22 -0400

    Added sass

--- app/root.tsx
+++ app/root.tsx
@@ -1,15 +1,37 @@
-import { Outlet, LiveReload } from '@remix-run/react';
-export default function App(){
+import { Outlet, LiveReload, Links } from "@remix-run/react";
+import globalStyles from '~/styles/global.css'
+//imports any stylesheet 
+export const links = () => [{rel:'stylesheet',href:'globalStyles'}]
+
+//This will hold all out of html meta info
+export default function App() {
   return (
-    <html lang='en'>
-      <head>
-        <title> Netflix </title>
-        </head>
-        <body>
-          <Outlet />
-          <h1> Jellybeans1</h1>
-          {process.env.NODE_ENV === 'development' ? <LiveReload/> : null}
-          </body>
-          </html>
-  )
+    <Document>
+      <Outlet />
+    </Document>
+  );
 }
+
+
+
+type Props = {
+  title?: string,
+  children: JSX.Element,
+}
+const Document = ({ children, title }: Props) => {
+  return (
+    <html lang="en">
+      <head>
+        <Links />
+        <title>{title ? title : 'Netflix - Watch TV Shows Online' }</title>
+      </head>
+      <body>
+        {children}
+        <h1> Jellybeans1</h1>
+        <button className='click'> Hello </button>
+        {process.env.NODE_ENV === "development" ? <LiveReload /> : null}
+      </body>
+    </html>
+  );
+};
+// no need for consistent component that renders on every page 
