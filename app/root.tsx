a62537ff5538d7e78b026763f82e4e33c20c4cb1
Drummer Dee <drummer.deja@gmail.com>
2022-09-17 22:41:59 -0400

    outline of Netflix landing page

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,5 @@
-import { Outlet, LiveReload, Links } from "@remix-run/react";
+import { Outlet, LiveReload, Links, Link } from "@remix-run/react";
+import Homepage from '~/routes';
 import globalStyles from '~/styles/global.css'
 //imports any stylesheet 
 export const links = () => [{rel:'stylesheet',href: globalStyles }]
@@ -7,16 +8,18 @@ export const links = () => [{rel:'stylesheet',href: globalStyles }]
 export default function App() {
   return (
     <Document>
-      <Outlet />
+      <Layout>
+        <Outlet />
+      </Layout>
     </Document>
   );
 }
 
 
 
-type Props = {
+interface Props {
   title?: string,
-  children: JSX.Element,
+  children: JSX.Element | JSX.Element[],
 }
 const Document = ({ children, title }: Props) => {
   return (
@@ -27,8 +30,6 @@ const Document = ({ children, title }: Props) => {
       </head>
       <body>
         {children}
-        <h1> Jellybeans1</h1>
-        <button className='click'> Hello </button>
         {process.env.NODE_ENV === "development" ? <LiveReload /> : null}
       </body>
     </html>
@@ -37,3 +38,20 @@ const Document = ({ children, title }: Props) => {
 // header and footer are components that wrap on most pages 
 // create separate components with transparent colors 
 // remember to watch the routing portion of the video
+
+const Layout = ({children }: Props) => {
+  return (
+    <>
+    <Link to='/'><img src ='#' alt='Netflix' /></Link>
+    <Link to='/signIn'> Sign In </Link>
+    <div>{children}</div>
+    </>
+  )
+}
+const footerLayout = ({children}: Props) => {
+  return (
+    <>
+    <h3> Questions? Call 1-800-NETFLIX </h3>
+    </>
+  )
+}
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,8 +1,38 @@
 import globalStyles from '~/Styles/global.css'
+import {Link} from '@remix-run/react';
 
 export default function Index() {
   return (
     <>
+    <main>
+    <h1> Unlimited movies, TV shows and more </h1>
+            <h3 className="text-red-500 hover:text-blue-500"> Watch anywhere. Cancel anytime </h3>
+            <p> Ready to watch? Enter your email to create or restart your membership. </p>
+            <div>
+                <form>
+                <input id="email" type='email' name='email'placeholder="Email Address" autoComplete="email"/>
+                <Link to='/signUp'> Get Started </Link>
+                </form>
+            </div>
+    </main>
+    <section>
+      <h1> Title of section </h1>
+      <p> small paragraph </p>
+      </section>
+    <section>
+      <h1> Title of section </h1>
+      <p> small paragraph </p>
+      </section>
+    <section>
+      <h1> Title of section </h1>
+      <p> small paragraph </p>
+      </section>
+    <section>
+      <h1> Title of section </h1>
+      <p> small paragraph </p>
+      </section>
+      {/* dropdown menu goes here */}
+      {/* have to figure out the footer layout here */}
     </>
   );
 }
